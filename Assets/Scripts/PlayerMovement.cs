deb00587e9228a9db9730b4c5b74d530f9f82cfb
CasenThurgood <142850150+CasenThurgood@users.noreply.github.com>
2026-04-12 15:09:42 -0600

    (60%) - Player Movement Updated - (none)
    Its pretty basic for now, but I made the movement configurable, but I may change it later to make it feel more responsive.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Security;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,18 +9,15 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float maxSpeed;
-    [SerializeField] private float sx;
     [SerializeField] private float friction;
+    [SerializeField] private Rigidbody2D rb;
     
-    [Header("Gravity")]
-    [SerializeField] private float maxFallSpeed;
-    [SerializeField] private float gravityLevel;
-    [SerializeField] private float sy;
+    [Header("Jumping")]
+    [SerializeField] private float jumpHeight;
+
+    [SerializeField] private LayerMask groundLayer;
     
     [SerializeField] private bool isGrounded;
-
-
-    public bool isSlippery = false;
 
     public InputAction playerControls;
 
@@ -34,58 +32,40 @@ public class PlayerMovement : MonoBehaviour
     {
         playerControls.Disable();
     }
-    
-    // Update is called once per frame
-    void Update()
+
+    void Start()
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
-        //Right
-        if (playerControls.ReadValue<Vector2>().x > 0.1f)
-        {
-            if (sx < 0)
-            {
-                sx = 0;
-            }
-            sx += moveSpeed * Time.deltaTime;
-        }
-        //Left
-        else if (playerControls.ReadValue<Vector2>().x < -0.1f)
-        {
-            if (sx > 0)
-            {
-                sx = 0;
-            }
-            sx -= moveSpeed * Time.deltaTime;
-        }
-        else
-        {
-            if (!isSlippery)
-            {
-                sx = 0;
-                return;
-            }
-            sx = Math.Sign(sx) * (Math.Abs(sx) - friction * Time.deltaTime);
-            if (sx < 0.1f && sx > -0.1f)
-            {
-                sx = 0;
-            }
-        }          
+        rb = GetComponent<Rigidbody2D>();
+        jumpHeight *= 300;
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, .5f, groundLayer);
+
+        float inputY = playerControls.ReadValue<Vector2>().y;
         
-        sx = Math.Clamp(sx, -maxSpeed, maxSpeed);
+        if (inputY > 0.01f && isGrounded)
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpHeight));
+            isGrounded = false;
+        }
+        
+        float input = playerControls.ReadValue<Vector2>().x;
 
-        if (playerControls.ReadValue<Vector2>().y > 0.1f && isGrounded)
-        {
-            sy = moveSpeed * 2 * Time.deltaTime;
-        }
-        if (!isGrounded)
-        {
-            sy -= gravityLevel * Time.deltaTime;
-            sy = Math.Clamp(sy, -maxFallSpeed, maxFallSpeed*2);
-        }
+        // target speed
+        float targetSpeed = input * maxSpeed;
+
+        // difference between current and target
+        float speedDif = targetSpeed - rb.linearVelocityX;
+
+        // acceleration rate
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? moveSpeed : friction;
+
+        // calculate force
+        float movement = speedDif * accelRate;
+
+        rb.AddForce(movement * Vector2.right);
     }
 
-    private void FixedUpdate()
-    {
-        transform.position += new Vector3(sx, 0, 0) * Time.fixedDeltaTime;
-    }
 }
